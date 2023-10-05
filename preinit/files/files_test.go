fe0c9c1633f20cfef6108d3311f8a369dad5c6a4
Joseph Wright <rjosephwright@gmail.com>
2023-10-05 12:48:11 -0700

    Add tests for Mkdirs() function

--- preinit/files/files_test.go
+++ preinit/files/files_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/spf13/afero"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -52,6 +53,64 @@ func Test_DescendingDirs(t *testing.T) {
 	for _, tc := range testCases {
 		actual := DescendingDirs(tc.dir)
 		assert.ElementsMatch(t, tc.result, actual)
+	}
+}
+
+func Test_Mkdirs(t *testing.T) {
+	testCases := []struct {
+		dir    string
+		err    error
+		result []string
+	}{
+		{
+			dir:    "",
+			err:    nil,
+			result: []string{},
+		},
+		{
+			dir:    "/",
+			err:    nil,
+			result: []string{"/"},
+		},
+		{
+			dir:    "//",
+			err:    nil,
+			result: []string{"/"},
+		},
+		{
+			dir: "/aaa/zzz/bbb/yyy",
+			err: nil,
+			result: []string{
+				"/",
+				"/aaa",
+				"/aaa/zzz",
+				"/aaa/zzz/bbb",
+				"/aaa/zzz/bbb/yyy",
+			},
+		},
+		{
+			dir: "/aaa///zzz//bbb/////yyy",
+			err: nil,
+			result: []string{
+				"/",
+				"/aaa",
+				"/aaa/zzz",
+				"/aaa/zzz/bbb",
+				"/aaa/zzz/bbb/yyy",
+			},
+		},
+	}
+	for _, tc := range testCases {
+		fs := afero.NewMemMapFs()
+		err := Mkdirs(fs, tc.dir, 0, 0, 755)
+		assert.Equal(t, tc.err, err)
+		for _, dir := range tc.result {
+			t.Run(fmt.Sprintf("directory %s", dir), func(t *testing.T) {
+				dirExists, err := afero.DirExists(fs, dir)
+				assert.Nil(t, err, "error was not nil: %s", err)
+				assert.True(t, dirExists, "directory %s does not exist", dir)
+			})
+		}
 	}
 }
 
